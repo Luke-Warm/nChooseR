a96408bc157f041762b472c287a41057d3050b65
behac6513 <behac6513@7HDN6Y1.Student.UGDSB.ED>
2016-11-24 13:35:57 -0500

    great commit

---
+++ src/n/chooser/r/NChooserR.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package n.chooser.r;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author behac6513
+ */
+public class NChooserR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
